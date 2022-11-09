faa0ca192b88ce019845abf28cc9b18b76d7a5e0
moizali-dev <moiz790@gmail.com>
2022-11-08 21:52:00 -0500

    day 4 delim str

--- day4/day4.c
+++ day4/day4.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // https://adventofcode.com/2021/day/4
 
@@ -12,22 +14,38 @@ int part1(void) {
     char binary[74];
     int callednumsArr[27];
     int i = 0;
+    int arr_i = 0;
 
     file = fopen("day4inputsample.txt", "r");
 
     while (!feof (file)) {
         fgets(userText, USER_TEXT_LIMIT, file);
         valuesRead = sscanf(userText, "%74s", binary);
+        
 
         if (valuesRead == 1 && i == 0) {
-            
-            // save the values in an array
-            
-        } elif (valuesRead == 1) {
 
-
+            // delimit from ","
+            char * token = strtok(binary, ",");
+            // loop through the string to extract all other tokens
+            while( token != NULL ) {
+                printf( "%s\n", token ); //printing each token
+                callednumsArr[arr_i] = atoi(token);
+                arr_i++;
+                token = strtok(NULL, ",");
+            }
+            //print the values of an array
+            for (int a = 0; a <= 27; a++){
+                printf("%d ",callednumsArr[a]);
+            }
+            
+        } else if (valuesRead == 1 && i != 0) {
+            
         }
-        printf("%d\n", valuesRead);
+
+        i++;
+
+        // printf("%d\n", valuesRead);
     }
 
     return 0;
